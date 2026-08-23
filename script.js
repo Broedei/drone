5525def6600aeee9765ae187ebf68db0162fe762
Broedei <d.wenker@outlook.com>
2026-08-23 19:48:21 +0200

    Enhance GPS functionality and alerts in script.js
    Refactor geolocation handling to improve error messages and user feedback.

--- script.js
+++ script.js
@@ -1,27 +1,45 @@
-alert("JAVASCRIPT WERKT!");
+alert("JavaScript werkt. GPS wordt nu aangevraagd...");
 
-navigator.geolocation.getCurrentPosition(
+if (!navigator.geolocation) {
 
-    function(position) {
+    alert("Deze browser ondersteunt geen GPS.");
 
-        alert(
-            "GPS werkt!\n\nLatitude: " +
-            position.coords.latitude +
-            "\nLongitude: " +
-            position.coords.longitude
-        );
+} else {
 
-    },
+    navigator.geolocation.getCurrentPosition(
 
-    function(error) {
+        function(position) {
 
-        alert(
-            "GPS fout:\n" +
-            error.message +
-            "\nCode: " +
-            error.code
-        );
+            alert(
+                "GPS WERKT!\n\n" +
+                "Latitude: " +
+                position.coords.latitude +
+                "\nLongitude: " +
+                position.coords.longitude +
+                "\nNauwkeurigheid: " +
+                position.coords.accuracy +
+                " meter"
+            );
 
-    }
+        },
 
-);
+        function(error) {
+
+            alert(
+                "GPS FOUT\n\n" +
+                "Code: " +
+                error.code +
+                "\nMelding: " +
+                error.message
+            );
+
+        },
+
+        {
+            enableHighAccuracy: true,
+            timeout: 30000,
+            maximumAge: 0
+        }
+
+    );
+}
