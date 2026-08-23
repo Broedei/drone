6fb8072ace2dc5ae37ea15c93ebbf144e07b607b
Broedei <d.wenker@outlook.com>
2026-08-23 19:46:48 +0200

    Add geolocation functionality with alerts

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 alert("JAVASCRIPT WERKT!");
+
+navigator.geolocation.getCurrentPosition(
+
+    function(position) {
+
+        alert(
+            "GPS werkt!\n\nLatitude: " +
+            position.coords.latitude +
+            "\nLongitude: " +
+            position.coords.longitude
+        );
+
+    },
+
+    function(error) {
+
+        alert(
+            "GPS fout:\n" +
+            error.message +
+            "\nCode: " +
+            error.code
+        );
+
+    }
+
+);
